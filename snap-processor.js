600ea9208e2242727a8b9744589d545c4652cfe7
nannal <d.abbott@nannal.com>
2019-04-15 16:31:08 +0300

    Indent fix

--- snap-processor.js
+++ snap-processor.js
@@ -1,316 +1,102 @@
-const http = require('http');
-const uuidv4 = require('uuid/v4');
-const shell = require('shelljs');
-const formidable = require('formidable');
-const readChunk = require('read-chunk');
-const fileType = require('file-type');
-var cmds = require('./snap-processor-cmds.js');
-
-corsVar = process.env.CORSVAR || 'https://dtube.nannal.com';
-
-// variable to assure only one upload request happens
-var reqhappened = false;
-
-// generated token
-const genToken = uuidv4();
+const ipfsAPI = require('ipfs-http-client');
+const fs = require('fs');
 
 
-http.createServer(function (req, res) {
+var ipfsIp = process.env.IPFSIP || '127.0.0.1';
+var ipfsPort = process.env.IPFSPORT || '5001';
+var ipfsProtocol = process.env.IPFSPROTOCOL || 'http';
 
+var cmds = {
 
-	res.setHeader('Access-Control-Allow-Credentials', 'true');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Accept');
-	res.setHeader('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
-	res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS');
-	res.setHeader('access-control-allow-origin', corsVar);
-	res.setHeader('Access-Control-Max-Age', '1728000');
-	res.setHeader('Connection', 'keep-alive');
-	// res.setHeader('Content-Type', 'application/json; charset=utf-8');
-	// res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+	shell_cmds: {
+		// make sure all files and directories exist where they should
+		createResizeCmd: (filePath) => {
+			return `convert ` + filePath + ` -resize 640x360 -gravity Center -crop 640x360 ./snap/resizedImg`
+		},
 
-<<<<<<< HEAD
-  if (req.url == '/getStatus') {
-<<<<<<< HEAD
-    res.statusCode = 200;
-    res.end('{"version":"0.7.5","currentWaitingInQueue":{"audioCpuToEncode":0,"videoGpuToEncode":0,"audioVideoCpuToEncode":0,"spriteToCreate":0,"ipfsToAdd":0}}');
-  };
-<<<<<<< HEAD
->>>>>>> 4234e7a... tabs to spaces
-=======
->>>>>>> 4234e7a11f59e442911f705f8fa1ada21100ffc8
->>>>>>> 6d00387... merge fix 2
-
-	// sending progress to user
-	if (req.url.match(/\/getProgressByToken.*/)) {
-	res.end(JSON.stringify(cmds.processResponse));
-=======
-=======
-	if (req.url == '/getStatus') {
->>>>>>> 769f1d3... Spaces to tabs
-		res.statusCode = 200;
-		res.end('{"version":"0.7.5","currentWaitingInQueue":{"audioCpuToEncode":0,"videoGpuToEncode":0,"audioVideoCpuToEncode":0,"spriteToCreate":0,"ipfsToAdd":0}}');
->>>>>>> fc4acba... fixed snap proc js
-	};
-
-<<<<<<< HEAD
-  // sending progress to user
-   if (req.url.match(/\/getProgressByToken.*/)) {
-     res.end(JSON.stringify(cmds.processResponse));
-   };
-
-
-
-<<<<<<< HEAD
-	if (req.url == '/uploadImage' && !reqhappened) {
-
-	if (req.method === 'OPTIONS'){
-	res.statusCode = 204;
-	res.end();
-
-	} else {
-
-	res.statusCode = 200;
-
-	reqhappened = true;
-	var form = new formidable.IncomingForm();
-
-<<<<<<< HEAD
-	//Sane Form options
-	form.maxFields = 1
-	form.encoding = 'utf-8';
-	form.maxFileSize = '1024000';
-
-	form.parse(req, function (err, fields, files) {
-	});
-
-	// file is moved to upload folder and renamed to uuid
-	form.on('fileBegin', function (name, file){
-	file.path = "./upload/" + genToken;
-	});
-=======
-      //Sane Form options
-      form.maxFields = 1
-      form.encoding = 'utf-8';
-      form.maxFileSize = '1024000';
-
-      form.parse(req, function (err, fields, files) {
-      });
-
-      // file is moved to upload folder and renamed to uuid
-      form.on('fileBegin', function (name, file){
-        file.path = "./upload/" + genToken;
-      });
-<<<<<<< HEAD
->>>>>>> 4234e7a... tabs to spaces
-=======
->>>>>>> 4234e7a11f59e442911f705f8fa1ada21100ffc8
->>>>>>> 6d00387... merge fix 2
-
-	form.on('file', function (name, file) {
-
-	//frontend needs to know if upload was successful and receive the token
-	var successResponse = { success: "", token: ""};
-	var allowedExtensions = ['jpg', 'jpeg', 'bmp', 'png'];
-
-	const buffer = readChunk.sync(file.path, 0, fileType.minimumBytes);
-
-	var uploadedFileType = fileType(buffer).ext;
-	console.log(uploadedFileType);
-
-	if (!allowedExtensions.includes(uploadedFileType)){
-
-<<<<<<< HEAD
-	// if not image, success is false, no token, end process
-	successResponse.success = "false";
-	res.end(JSON.stringify(successResponse));
-	process.exit();
-=======
-          // if not image, success is false, no token, end process
-          successResponse.success = "false";
-          res.end(JSON.stringify(successResponse));
-          process.exit();
-<<<<<<< HEAD
->>>>>>> 4234e7a... tabs to spaces
-=======
->>>>>>> 4234e7a11f59e442911f705f8fa1ada21100ffc8
->>>>>>> 6d00387... merge fix 2
-
-	} else {
-
-<<<<<<< HEAD
-	// if file is valid, success is true and provide token
-	successResponse.success = "true";
-	successResponse.token = genToken;
-	res.end(JSON.stringify(successResponse));
-
-	// resize the image with shell command
-	shell.exec(cmds.shell_cmds.createResizeCmd(file.path), function(code, stdout, stderr) {
-	// if code aint 0 there's an error
-	if (code) {
-	console.log(stderr);
-	process.exit();
-	}
-	// upload resized (called source for some reason) image to ipfs
-	cmds.ipfs_cmds.ipfsUpload("./snap/resizedImg", "ipfsAddSource");
-=======
-          // if file is valid, success is true and provide token
-          successResponse.success = "true";
-          successResponse.token = genToken;
-          res.end(JSON.stringify(successResponse));
-
-          // resize the image with shell command
-          shell.exec(cmds.shell_cmds.createResizeCmd(file.path), function(code, stdout, stderr) {
-            // if code aint 0 there's an error
-            if (code) {
-              console.log(stderr);
-              process.exit();
-            }
-            // upload resized (called source for some reason) image to ipfs
-            cmds.ipfs_cmds.ipfsUpload("./snap/resizedImg", "ipfsAddSource");
-<<<<<<< HEAD
->>>>>>> 4234e7a... tabs to spaces
-=======
->>>>>>> 4234e7a11f59e442911f705f8fa1ada21100ffc8
->>>>>>> 6d00387... merge fix 2
-=======
->>>>>>> fc4acba... fixed snap proc js
-
-   if (req.url == '/uploadImage' && !reqhappened) {
-
-         if (req.method === 'OPTIONS'){
-             res.statusCode = 204;
-             res.end();
-=======
-	// sending progress to user
-	if (req.url.match(/\/getProgressByToken.*/)) {
-		res.end(JSON.stringify(cmds.processResponse));
-	};
->>>>>>> 769f1d3... Spaces to tabs
-
-
-
-
-	if (req.url == '/uploadImage' && !reqhappened) {
-
-		if (req.method === 'OPTIONS'){
-			res.statusCode = 204;
-			res.end();
-
-		} else {
-
-			res.statusCode = 200;
-
-			reqhappened = true;
-			var form = new formidable.IncomingForm();
-
-			//Sane Form options
-			form.maxFields = 1
-			form.encoding = 'utf-8';
-			form.maxFileSize = '1024000';
-
-			form.parse(req, function (err, fields, files) {
-			});
-
-			// file is moved to upload folder and renamed to uuid
-			form.on('fileBegin', function (name, file){
-				file.path = "./upload/" + genToken;
-			});
-
-			form.on('file', function (name, file) {
-
-				//frontend needs to know if upload was successful and receive the token
-				var successResponse = { success: "", token: ""};
-				var allowedExtensions = ['jpg', 'jpeg', 'bmp', 'png'];
-
-				const buffer = readChunk.sync(file.path, 0, fileType.minimumBytes);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	form.on('error', function(err) {
-	console.error('Error', err)
-	process.exit();
-	});
-=======
-      form.on('error', function(err) {
-        console.error('Error', err)
-        process.exit();
-      });
-<<<<<<< HEAD
->>>>>>> 4234e7a... tabs to spaces
-=======
->>>>>>> 4234e7a11f59e442911f705f8fa1ada21100ffc8
->>>>>>> 6d00387... merge fix 2
-=======
-                    } else {
-=======
-				var uploadedFileType = fileType(buffer).ext;
-				console.log(uploadedFileType);
->>>>>>> 769f1d3... Spaces to tabs
-
-				if (!allowedExtensions.includes(uploadedFileType)){
-
-					// if not image, success is false, no token, end process
-					successResponse.success = "false";
-					res.end(JSON.stringify(successResponse));
-					process.exit();
-
-				} else {
-
-					// if file is valid, success is true and provide token
-					successResponse.success = "true";
-					successResponse.token = genToken;
-					res.end(JSON.stringify(successResponse));
-
-					// resize the image with shell command
-					shell.exec(cmds.shell_cmds.createResizeCmd(file.path), function(code, stdout, stderr) {
-						// if code aint 0 there's an error
-						if (code) {
-							console.log(stderr);
-							process.exit();
-						}
-						// upload resized (called source for some reason) image to ipfs
-						cmds.ipfs_cmds.ipfsUpload("./snap/resizedImg", "ipfsAddSource");
-
-						// overlay dtube logo on image with shell command
-						shell.exec(cmds.shell_cmds.createOverlayCmd("./snap/resizedImg"), function(code, stdout, stderr){
-							// if code aint 0 there's an error
-							if (code) {
-								console.log(stderr);
-								process.exit();
-							}
-							// upload overlayed image to ipfs
-							cmds.ipfs_cmds.ipfsUpload("./snap/overlayedImg", "ipfsAddOverlay");
-
-							// check if finished and exit if done.
-							cmds.checkIfFinished();
-
-						});
-
-					});
-
-				}
-
-			});
-
-<<<<<<< HEAD
-            }
->>>>>>> fc4acba... fixed snap proc js
-=======
-			form.on('error', function(err) {
-				console.error('Error', err)
-				process.exit();
-			});
->>>>>>> 769f1d3... Spaces to tabs
-
+		// make sure all files and directories exist where they should
+		createOverlayCmd: (filePath) => {
+			return `composite -gravity NorthEast overlay.png ` + filePath +` ./snap/overlayedImg`
 		}
 
-	} else {
-		res.end("There's nothing here for you");
+	},
+
+	ipfs_cmds: {
+
+		// uploads file to ipfs, second parameter is the property to update within process response
+		ipfsUpload: (filePath, prop) => {
+			//Connceting to our http api
+			const ipfs = ipfsAPI(ipfsIp, ipfsPort, {protocol: ipfsProtocol})
+			let fileToUpload = fs.readFileSync(filePath);
+			let testBuffer = new Buffer.from(fileToUpload);
+
+			ipfs.add(testBuffer, function (err, file) {
+
+				if (err) {
+					console.log(err);
+					process.exit();
+				}
+				// updating relevant process response fields
+				cmds.setObjPropToValue(cmds.processResponse, prop+".progress", "100.00%");
+				cmds.setObjPropToValue(cmds.processResponse, prop+".lastTimeProgress", Date());
+				cmds.setObjPropToValue(cmds.processResponse, prop+".step", "success");
+				cmds.setObjPropToValue(cmds.processResponse, prop+".hash", file[0].hash);
+				cmds.setObjPropToValue(cmds.processResponse, prop+".fileSize", file[0].size);
+
+			});
+
+		}
+	},
+
+	// function for setting deep nested object property values
+	setObjPropToValue: (obj, path, value) => {
+		var i;
+		path = path.split(/(?:\.|\[|\])+/);
+		for (i = 0; i < path.length - 1; i++)
+			obj = obj[path[i]];
+
+		obj[path[i]] = value;
+	},
+
+	checkIfFinished: () => {
+
+		var func = setInterval(()=>{
+
+			if (cmds.processResponse.ipfsAddSource.progress == "100.00%" && cmds.processResponse.ipfsAddOverlay.progress == "100.00%"){
+				clearInterval(func);
+				// wait before ending process
+				setTimeout(()=>{
+					process.exit();
+				},20000);
+			}
+
+		},2000);
+
+	},
+
+	processResponse: {
+		"ipfsAddSource": {
+			"progress": "0.00%",
+			"encodeSize": "source",
+			"lastTimeProgress": null,
+			"errorMessage": null,
+			"step": "Started",
+			"positionInQueue": null,
+			"hash": null,
+			"fileSize": null
+		},
+		"ipfsAddOverlay": {
+			"progress": "Waiting in queue...",
+			"encodeSize": "source",
+			"lastTimeProgress": null,
+			"errorMessage": null,
+			"step": "Waiting",
+			"positionInQueue": 1,
+			"hash": null,
+			"fileSize": null
+		}
 	}
 
-}).listen(5000, ()=> {
+}
 
-	console.log("listening on port 5000");
-
-});
+module.exports = cmds
